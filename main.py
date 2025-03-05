4af3f5c04ea99fa8a2e7fb86b6b852c5b98e8249
sindri1111 <sindris21@ru.is>
2025-03-05 13:07:00 +0000

    sindri bleh

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def main():
     tests.test_undo()
     tests.test_deterministic(3, 20)
     
-    # tests.test_deterministic_graph()
-    # tests.test_deterministic_graph(retain_tree=False)
+    tests.test_deterministic_graph()
+    tests.test_deterministic_graph(retain_tree=False)
+
     # tests.test_random_graph()
     # tests.test_random_graph(retain_tree=False)
-        
 
 main()
