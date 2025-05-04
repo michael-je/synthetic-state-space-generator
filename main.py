ac52f22a9a3337ed8b9a09def4ea5fd022324eae
sindri1111 <sindris21@ru.is>
2025-05-04 17:03:36 +0000

    testing minimax search

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 import tests
+from State import State
 
+INF = 1000
+
+def minimax(state: State, depth: int, isMaximizingPlayer: bool):
+    if state.is_terminal():
+        return state.value()
+    if depth == 0:
+        return state.heuristic_value()
+
+    if isMaximizingPlayer:
+        maxEval = -INF
+        for action in state.actions():
+            state.make(action)
+            eval = minimax(state, depth-1, not isMaximizingPlayer)
+            state.undo()
+            maxEval = max(maxEval, eval)
+        return maxEval
+    else:
+        minEval = INF
+        for action in state.actions():
+            state.make(action)
+            eval = minimax(state, depth-1, not isMaximizingPlayer)
+            state.undo()
+            minEval = min(minEval, eval)
+
+        return minEval
 
 def main():
-    # tests.test_hash_average(1000000)
-    tests.test_deterministic_graph_2()
-    # tests.test_random_graph()
-    # tests.test_ids()
-        
+    for i in range(100):
+        state = State(max_depth=10, seed=i)
+
+        val1 = minimax(state, 5, isMaximizingPlayer=True)
+        val2 = minimax(state, 5, isMaximizingPlayer=False)
+        print(val1, val2)
+
+
 
 main()
