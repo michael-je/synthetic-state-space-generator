924e8e36d27b83d998c6e70837ed4b6711aef997
Your Name <michaeljoseph195@gmail.com>
2025-05-12 18:13:06 +0000

    clean up example functions

--- example_functions.py
+++ example_functions.py
@@ -6,10 +6,8 @@ def branching_function_midgame_heavy(randint: RandomIntFunction, randf: RandomFl
     x = 2 * params.self.depth / params.globals.max_depth
     y = -(x-1)**2 + 1
     branching_factor = y * params.globals.branching_factor_base
-    
     variance = (randf() - 0.5) * 2 * params.globals.branching_factor_variance
     branching_factor = int(max(0, branching_factor + round(variance)))
-    
     # make sure we do not prematurely create a terminal
     if params.self.depth < params.globals.terminal_minimum_depth:
         branching_factor = min(1, branching_factor)
@@ -22,20 +20,10 @@ def branching_function_simple_constant(randint: RandomIntFunction, randf: Random
 
 
 def child_depth_function_random(randint: RandomIntFunction, randf: RandomFloatFunction, params: StateParams) -> int:
-    """Generate children anywhere in the entire state space."""
+    """Generate children at any random depth."""
     return randint(low=0, high=params.globals.max_depth)
 
 
-def child_depth_function_cycles_allowed(randint: RandomIntFunction, randf: RandomFloatFunction, params: StateParams) -> int:
-    """Allows children to create cycles by a few levels."""
-    if params.parent is None:
-        return 1
-    if randf() < params.globals.cycle_chance:
-        return int(max(1, params.self.depth - 3))
-    return params.self.depth + 1
-
-
-def transposition_space_function_simple_constant(randint: RandomIntFunction, randf: RandomFloatFunction, max_depth: int) -> dict[int, int]:
-    """Example showing only 5 possible transpositions per depth."""
-    max_states_per_depth = 10
-    return {d: max_states_per_depth for d in range(max_depth)}
+def transposition_space_function_simple_constant(randint: RandomIntFunction, randf: RandomFloatFunction, max_depth: int) -> int:
+    """Allow only exactly 100 different states per depth."""
+    return 100
