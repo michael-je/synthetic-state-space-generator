5b25c9c5ebf60b260e6fbf118b04003d78dd19ad
hrafno <112639274+hrafno@users.noreply.github.com>
2025-05-14 17:44:55 +0000

    Connect-4 example graphs added

--- examples/example_behavior_functions.py
+++ examples/example_behavior_functions.py
@@ -27,3 +27,67 @@ def child_depth_function_random(randint: RandomIntFunction, randf: RandomFloatFu
 def transposition_space_function_simple_constant(randint: RandomIntFunction, randf: RandomFloatFunction, max_depth: int) -> int:
     """Allow only exactly 100 different states per depth."""
     return 100
+
+
+def connect_four_branching_function_complex(randint: RandomIntFunction, randf: RandomFloatFunction, params: StateParams) -> int:
+    """Uses the statistics from the real game to explicitly make the branching factor behave correctly in the synthetic game graph.
+    Only has information on the first 10 levels of Connect-4, and only serves as a proof of concept."""
+    rand_num = randf()
+    if params.self.depth < 6 :
+        return 7
+    elif params.self.depth == 6 :
+        if rand_num < 7/(7+16415):
+            return 6
+        return 7
+    elif params.self.depth == 7 :
+        if rand_num < 294/(294+728+53837):
+            return 6
+        elif rand_num < (294+728)/(294+728+53837):
+            return 0
+        return 7
+    elif params.self.depth == 8 :
+        if rand_num < 4326/(4326+1892+178057):
+            return 6
+        elif rand_num <  (4326+1892)/(4326+1892+178057):
+            return 0
+        return 7
+    elif params.self.depth == 9 :
+        if rand_num < 31984/(31984+19412+506790):
+            return 6
+        elif rand_num <  (31984+19412)/(31984+19412+506790):
+            return 0
+        return 7
+    else:
+        if rand_num < 157734/(157734+4425+1460664):
+            return 6
+        elif rand_num < (157734+44225)/(157734+44225+1460664):
+            return 0
+        return 7
+
+
+
+def connect_four_transposition_space_function_complex(randint: RandomIntFunction, randf: RandomFloatFunction, globals: GlobalVariables, depth: int) -> int:
+    """Uses the statistics from the real game to explicitly make the transpositon space behave correctly in the synthetic game graph.
+    Only has information on the first 10 levels of Connect-4, but serves as a proof of concept"""
+    trans_map = {0: 1,
+                 1: 100000,  # Large transposition space size at this level, so likely no transpositions
+                 2: 100000,  # Large transposition space size at this level, so likely no transpositions
+                 3: 438,     # Having the size of the transposition space as 438, gives us expected 238 unique states, which is derived from the real game.
+                 4: 1950,    # Having the size of the transposition space as 1950, gives us expected 1120 unique states, which is derived from the real game.
+                 5: 5708,    # gets us the expected number of unique states, derived from the real game of Connect-4
+                 6: 22209,   # gets us the expected number of unique states, derived from the real game of Connect-4
+                 7: 66822,   # gets us the expected number of unique states, derived from the real game of Connect-4
+                 8: 227191,  # gets us the expected number of unique states, derived from the real game of Connect-4
+                 9: 649959,  # gets us the expected number of unique states, derived from the real game of Connect-4
+                 10: 1662623 # gets us the expected number of unique states, derived from the real game of Connect-4
+                 }
+    
+    return trans_map[depth]
+
+
+def connect_four_transposition_space_function_simple(randint: RandomIntFunction, randf: RandomFloatFunction, globals: GlobalVariables, depth: int) -> int:
+    """Simple transposition space function, which uses an estimation of the transposition-space at each level"""
+    if depth <= 1:
+        return 100000 # No transpositions when depth=0 and depth=1.
+    else:
+        return (4 ** (depth - 2)) * (7 ** 2) # Estimation of the state-space at each depth of the graph.
--- examples/example_graphs.py
+++ examples/example_graphs.py
@@ -1,4 +1,29 @@
 from sssg.SyntheticGraph import SyntheticGraph
+from examples.example_behavior_functions import *
 
 
 state = SyntheticGraph()
+
+
+def connect_four_example_simple():
+    """Example creation of synthetic Connect-4 graph. This is a simple and naive approximation of the graph.
+    For more detailed and accurate Connect-4 graph, see the connect_four_example_complex() function"""
+    state = SyntheticGraph(
+        max_depth = 42, #Maximum depth of Connect-4 graph is 42
+        branching_factor_base = 7, #Estimating the branching factor to be constant 7. 
+        transposition_space_function = connect_four_transposition_space_function_simple, # Simple function which 
+        terminal_minimum_depth=7, #Assuming th
+        terminal_chance=0.01, #Assuming that 1% of states after the terminal minimum depth are terminal stats
+        symmetry_factor=0.5, #Double symmetry due to mirror along the vertical center line
+        symmetry_frequency=0.01, #Assuming that 1% states are reflections
+    )
+
+def connect_four_example_complex():
+    """More complex version of the synthetic Connect-4 graph. This was made by studying how the Connect-4 game-tree behaves, 
+    and uses functions where detailed and explicit branching factor and transposition space have been calculated and implemented.
+    """
+    state = SyntheticGraph(
+        max_depth = 42,
+        branching_function = connect_four_branching_function_complex,
+        transposition_space_function = connect_four_transposition_space_function_complex,
+    )
