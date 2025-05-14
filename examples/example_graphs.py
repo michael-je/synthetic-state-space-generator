02ccc819b41bd962da6deee07acfc89ce1c06ecf
Michael Joseph <michaeljoseph195@gmail.com>
2025-05-14 18:41:07 +0000

    formatting

--- examples/example_graphs.py
+++ examples/example_graphs.py
@@ -3,8 +3,9 @@ from examples.example_behavior_functions import *
 
 
 def connect_four_example_simple():
-    """Example creation of synthetic Connect-4 graph. This is a simple and naive approximation of the graph.
-    For more detailed and accurate Connect-4 graph, see the connect_four_example_complex() function"""
+    """Example creation of synthetic Connect-4 graph. This is a simple and naive 
+    approximation of the graph.  For more detailed and accurate Connect-4 graph, see the 
+    connect_four_example_complex() function"""
     state = SyntheticGraph(
         max_depth = 42,
         branching_factor_base = 7,
@@ -16,9 +17,9 @@ def connect_four_example_simple():
     )
 
 def connect_four_example_complex():
-    """More complex version of the synthetic Connect-4 graph. This was made by studying how the Connect-4 game-tree behaves, 
-    and uses functions where detailed and explicit branching factor and transposition space have been calculated and implemented.
-    """
+    """More complex version of the synthetic Connect-4 graph. This was made by studying 
+    how the Connect-4 game-tree behaves, and uses functions where detailed and explicit 
+    branching factor and transposition space have been calculated and implemented."""
     state = SyntheticGraph(
         max_depth = 42,
         branching_function = branching_function_connect_four_complex,
