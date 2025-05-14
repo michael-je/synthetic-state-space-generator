98399266007d5a7c037562995ca87d56b236dfa0
Michael Joseph <michaeljoseph195@gmail.com>
2025-05-14 18:07:16 +0000

    refactor examples

--- examples/example_graphs.py
+++ examples/example_graphs.py
@@ -2,20 +2,17 @@ from sssg.SyntheticGraph import SyntheticGraph
 from examples.example_behavior_functions import *
 
 
-state = SyntheticGraph()
-
-
 def connect_four_example_simple():
     """Example creation of synthetic Connect-4 graph. This is a simple and naive approximation of the graph.
     For more detailed and accurate Connect-4 graph, see the connect_four_example_complex() function"""
     state = SyntheticGraph(
-        max_depth = 42, #Maximum depth of Connect-4 graph is 42
-        branching_factor_base = 7, #Estimating the branching factor to be constant 7. 
-        transposition_space_function = connect_four_transposition_space_function_simple, # Simple function which 
-        terminal_minimum_depth=7, #Assuming th
-        terminal_chance=0.01, #Assuming that 1% of states after the terminal minimum depth are terminal stats
-        symmetry_factor=0.5, #Double symmetry due to mirror along the vertical center line
-        symmetry_frequency=0.01, #Assuming that 1% states are reflections
+        max_depth = 42,
+        branching_factor_base = 7,
+        transposition_space_function = transposition_space_function_connect_four_simple,
+        terminal_minimum_depth=7,
+        terminal_chance=0.01,    # Assuming that 1% of states after the terminal minimum depth are terminal stats
+        symmetry_factor=0.5,     # Double symmetry due to mirroring along the vertical center line
+        symmetry_frequency=0.01, # Assuming that 1% states are reflections
     )
 
 def connect_four_example_complex():
@@ -24,6 +21,6 @@ def connect_four_example_complex():
     """
     state = SyntheticGraph(
         max_depth = 42,
-        branching_function = connect_four_branching_function_complex,
-        transposition_space_function = connect_four_transposition_space_function_complex,
+        branching_function = branching_function_connect_four_complex,
+        transposition_space_function = transposition_space_function_connect_four_complex,
     )
