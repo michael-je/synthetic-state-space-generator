3398e6585846060393b947b524c34627067ae3e1
Michael Joseph <michaeljoseph195@gmail.com>
2025-05-15 18:31:21 +0000

    fix whitespace

--- examples/example_graphs.py
+++ examples/example_graphs.py
@@ -13,15 +13,15 @@ state = SyntheticGraph(
     symmetry_factor=0.25,     # Symmetry works best when modelled to reflect around 4 axes
     symmetry_frequency=0.2,   # Assuming that 20% of states are reflections
     terminal_minimum_depth=5, # There are no wins/losses until at least turn 5
-    terminal_chance = 0.75)   # After that, the terminal chance is estimated to be around 75%
+    terminal_chance=0.75)   # After that, the terminal chance is estimated to be around 75%
 
 
 """Example creation of synthetic Connect-4 graph. This is a simpler approximation
 than the more complex example below."""
 state = SyntheticGraph(
-    max_depth = 42,
-    branching_factor_base = 7,
-    transposition_space_function = transposition_space_function_connect_four_simple,
+    max_depth=42,
+    branching_factor_base=7,
+    transposition_space_function=transposition_space_function_connect_four_simple,
     terminal_minimum_depth=7,
     terminal_chance=0.01,    # Assuming that 1% of states after the terminal minimum depth are terminal stats
     symmetry_factor=0.5,     # Double symmetry due to mirroring along the vertical center line
@@ -32,9 +32,9 @@ state = SyntheticGraph(
 how the Connect-4 game-tree behaves, and uses functions where detailed and explicit 
 branching factor and transposition space have been calculated and implemented."""
 state = SyntheticGraph(
-    max_depth = 42,
-    branching_function = branching_function_connect_four_complex,
-    transposition_space_function = transposition_space_function_connect_four_complex)
+    max_depth=42,
+    branching_function=branching_function_connect_four_complex,
+    transposition_space_function=transposition_space_function_connect_four_complex)
 
 
 """Example implementation of a "P-game with critical moves" as described in 
