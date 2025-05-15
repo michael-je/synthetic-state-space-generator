a323e93975d8a5f567d486a28efcaa65768e9045
Michael Joseph <michaeljoseph195@gmail.com>
2025-05-15 13:36:14 +0000

    fix typos

--- examples/example_usage.py
+++ examples/example_usage.py
@@ -7,7 +7,7 @@ def bfs(state: SyntheticGraph):
 	"""This function explores the graph in BFS order. The `set_root` method is used 
 	to move the internal state to the node being processed so its children 
 	can be generated. The original root node's ID is saved and restored 
-	at the end to preserve original sttate"""
+	at the end to preserve the original state"""
 	visited: set[int] = set()
 	queue: deque[int] = deque()
 	queue.append(state.id())
@@ -30,9 +30,9 @@ def bfs(state: SyntheticGraph):
 INF = 1000
 visited: dict[int, float|int] = {}
 def minimax(state: SyntheticGraph, depth: int) -> float|int:
-	"""This is a classic minimax search (no alpha beta pruning). It calculates
-	   the true value of a given state up to depth d. If it doesn't reach a 
-	   terminal node and were at the maximum depth capacity we return the heuristic."""
+	"""This is a classic minimax search (without Alpha-Beta pruning). It calculates
+	the true value of a given state up to depth d. If it doesn't reach a terminal node 
+	and were at the maximum depth capacity we return the heuristic."""
 	if state.id() in visited.keys():
 		return state.true_value()
 	if state.is_terminal():
